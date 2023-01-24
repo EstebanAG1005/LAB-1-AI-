a002d85859d056a0a6ff303ced0b1a875ee914c1
JuanCarlosBajan <baj20109@uvg.edu.gt>
2023-01-24 16:39:52 -0600

    comments bfs

--- algorithms.py
+++ algorithms.py
@@ -1,5 +1,3 @@
-import heapq
-
 
 def up(x, y, laberinto):
     if y < len(laberinto) - 1 and laberinto[y + 1][x] != 1:
@@ -32,53 +30,62 @@ def right(x, y, laberinto):
 def shortest_path_bfs(node1, node2, laberinto):
     path_list = [[node1]]
     path_index = 0
-    # To keep track of previously visited nodes
+    # Con el objetivo de tener un registro de todos los nodos anteriores
     previous_nodes = [node1]
     if node1 in node2:
         return path_list[0]
 
+    # Iteramos sobre todos los caminos posibles
     while path_index < len(path_list):
         current_path = path_list[path_index]
         last_node = current_path[-1]
         next_nodes = []
 
+        # Se buscan todos los nodos siguientes, en caso de este sistema, todos los pixeles al rededor
         next_nodes.append(up(last_node[0], last_node[1], laberinto))
         next_nodes.append(down(last_node[0], last_node[1], laberinto))
         next_nodes.append(left(last_node[0], last_node[1], laberinto))
         next_nodes.append(right(last_node[0], last_node[1], laberinto))
-        # Search goal node
+        
+        # Verificamos si el nodo destino se encuentra en uno de los nodos conectores
         for dest in node2:
             if dest in next_nodes:
                 current_path.append(dest)
                 return current_path
-        # Add new paths
+
+        # Agregamos nuevos caminos
         for next_node in next_nodes:
             if next_node != None and not next_node in previous_nodes:
                 new_path = current_path[:]
                 new_path.append(next_node)
                 path_list.append(new_path)
-                # To avoid backtracking
+                # mantenemos registro de los nodos visitados para evitar backtracking
                 previous_nodes.append(next_node)
-        # Continue to next path in list
+        # Continuamos el siguiente camino
         path_index += 1
-    # No path is found
+    # Si no encontramos ningun camino
     return []
 
 
-def bfs(node, visited, queue, laberinto):  # function for BFS
+def bfs(node, visited, queue, laberinto):
     visited.append(node)
     queue.append(node)
     found = False
-    while queue and not found:  # Creating loop to visit each node
+    #Vamos iterando sobre cada uno de los nodos
+    while queue and not found:
         m = queue.pop(0)
 
+        # Llevamos control de todos los nodos visitados
         neighbours = []
 
+        # Se buscan todos los nodos siguientes, en caso de este sistema, todos los pixeles al rededor
         neighbours.append(up(m[0], m[1], laberinto))
         neighbours.append(down(m[0], m[1], laberinto))
         neighbours.append(left(m[0], m[1], laberinto))
         neighbours.append(right(m[0], m[1], laberinto))
 
+        # Por cada uno de los siguientes nodos verificamos si estan en los visitados para evitar backtracking
+        # Tambien verificamos si es nodo final
         for neighbour in neighbours:
             if neighbour != None and neighbour not in visited:
                 if laberinto[neighbour[1]][neighbour[0]] != 2:
@@ -87,6 +94,7 @@ def bfs(node, visited, queue, laberinto):  # function for BFS
                 else:
                     found = True
                     break
+    # Retornamos todos los nodos visitados
     return visited
 
 
